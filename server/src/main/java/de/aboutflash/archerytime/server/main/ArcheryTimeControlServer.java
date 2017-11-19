2da61ab0a88996f9952196f94b799576e7e9331c
Falk von Zydowitz <falk@aboutflash.de>
2017-11-19 21:19:55 +0100

    Added server module

---
+++ server/src/main/java/de/aboutflash/archerytime/server/main/ArcheryTimeControlServer.java
@@ -0,0 +1,16 @@
+package de.aboutflash.archerytime.server.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Class
+ *
+ * @author falk@aboutflash.de on 19.11.2017.
+ */
+public class ArcheryTimeControlServer extends Application {
+  @Override
+  public void start(final Stage primaryStage) throws Exception {
+
+  }
+}
