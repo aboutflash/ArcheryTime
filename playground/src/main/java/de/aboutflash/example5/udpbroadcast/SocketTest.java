1ad280138584544a879b3ec087f7540f2c80d288
Falk von Zydowitz <falk@aboutflash.de>
2017-11-26 14:01:13 +0100

    Added working socket server/client example.

---
+++ playground/src/main/java/de/aboutflash/example5/udpbroadcast/SocketTest.java
@@ -0,0 +1,71 @@
+package de.aboutflash.example5.udpbroadcast;
+
+/**
+ * Class
+ *
+ * @author falk@aboutflash.de on 26.11.2017.
+ */
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class SocketTest {
+  public static void main(String[] args) throws IOException {
+
+    startServer();
+    startSender();
+  }
+
+  public static void startSender() {
+    (new Thread() {
+      @Override
+      public void run() {
+        try {
+          Socket s = new Socket("localhost", TransmissionThread.RESPONSE_PORT);
+          BufferedWriter out = new BufferedWriter(
+              new OutputStreamWriter(s.getOutputStream()));
+
+          while (true) {
+            out.write("Hello World!");
+            out.newLine();
+            out.flush();
+
+            Thread.sleep(200);
+          }
+
+        } catch (UnknownHostException e) {
+          e.printStackTrace();
+        } catch (IOException e) {
+          e.printStackTrace();
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    }).start();
+  }
+
+  public static void startServer() {
+    (new Thread() {
+      @Override
+      public void run() {
+        ServerSocket ss;
+        try {
+          ss = new ServerSocket(TransmissionThread.RESPONSE_PORT);
+
+          Socket s = ss.accept();
+
+          BufferedReader in = new BufferedReader(
+              new InputStreamReader(s.getInputStream()));
+          String line = null;
+          while ((line = in.readLine()) != null) {
+            System.out.println(line);
+          }
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }).start();
+  }
+}
